94427b6fb2702f1a95ae01a159f9ce2597cf2e38
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-22 11:55:33 -0500

    back para crear categorias

---
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [MaxLength(50, ErrorMessage ="No pude ser mayor a {1} caracteres")]
+        public string Nombre { get; set; }
+        public TipoOperacion TipoOperacionId { get; set; }
+        public int usuarioId { get; set; }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoOperacion.cs
@@ -0,0 +1,8 @@
+﻿namespace ManejoPresupuesto.Models
+{
+    public enum TipoOperacion
+    {
+        ingreso = 1,
+        Gasto = 2,  
+    }
+}

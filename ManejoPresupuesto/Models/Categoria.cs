bc2498f4e6528834a5a64d2383bb9c76c8a3ff66
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-22 12:05:42 -0500

    front crear categoria

--- ManejoPresupuesto/Models/Categoria.cs
+++ ManejoPresupuesto/Models/Categoria.cs
@@ -8,6 +8,7 @@ namespace ManejoPresupuesto.Models
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [MaxLength(50, ErrorMessage ="No pude ser mayor a {1} caracteres")]
         public string Nombre { get; set; }
+        [Display(Name = "Categoria")]
         public TipoOperacion TipoOperacionId { get; set; }
         public int usuarioId { get; set; }
     }

1f8d7f05b59285f806409ef1021cb1a05b5069c8
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-26 13:21:16 -0500

    back de creacion de transacciones

---
+++ ManejoPresupuesto/Models/Transaccion.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Transaccion
+    {
+        public int Id { get; set; }
+        public int usuarioId { get; set; }
+        public DateTime FechaTransaccion { get; set; } = DateTime.Today;
+        public decimal Monto { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Debe seleccionar una categoría")]
+        public int CategoriaId { get; set; }
+
+        public string Nota { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Debe seleccionar una categoría")]
+        public int CuentaId { get; set; }
+    }
+}

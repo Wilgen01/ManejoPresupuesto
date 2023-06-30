99f0579f713c903f20affee819abfc67333bdedc
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-30 18:38:22 -0500

    errores en español

---
+++ ManejoPresupuesto/Servicios/MensajesDeErrorIdentity.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public class MensajesDeErrorIdentity : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError()
+        {
+            return new IdentityError { Code = nameof(DefaultError), Description = "Se ha producido un error en el proceso de identidad." };
+        }
+
+        public override IdentityError ConcurrencyFailure()
+        {
+            return new IdentityError { Code = nameof(ConcurrencyFailure), Description = "Error de concurrencia. Los datos han sido modificados por otro usuario." };
+        }
+
+        public override IdentityError PasswordMismatch()
+        {
+            return new IdentityError { Code = nameof(PasswordMismatch), Description = "La contraseña proporcionada no coincide." };
+        }
+
+        public override IdentityError InvalidToken()
+        {
+            return new IdentityError { Code = nameof(InvalidToken), Description = "El token proporcionado es inválido." };
+        }
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return new IdentityError { Code = nameof(InvalidUserName), Description = $"El nombre de usuario '{userName}' no es válido." };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError { Code = nameof(DuplicateEmail), Description = $"Ya existe un usuario registrado con el correo electrónico '{email}'." };
+        }
+
+        public override IdentityError InvalidEmail(string email)
+        {
+            return new IdentityError { Code = nameof(InvalidEmail), Description = $"La dirección de correo electrónico '{email}' no es válida." };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "La contraseña debe contener al menos un carácter no alfanumérico." };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "La contraseña debe contener al menos un dígito." };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "La contraseña debe contener al menos una letra minúscula." };
+        }
+    }
+}

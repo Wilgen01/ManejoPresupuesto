90c8912b6b9d1b0350f21b3b72e69e34fa5f71dc
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-06-30 17:36:12 -0500

    validaciones en el modelo usuarios

--- ManejoPresupuesto/Models/RegistroViewModel.cs
+++ ManejoPresupuesto/Models/RegistroViewModel.cs
@@ -1,8 +1,18 @@
-﻿namespace ManejoPresupuesto.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
 {
     public class RegistroViewModel
     {
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo {0} debe ser un correo valido")]
+        [Display(Name = "Correo")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Contraseña")]
         public string PassWord { get; set; }
     }
 }
